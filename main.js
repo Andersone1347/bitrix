b6c64e3e1707377173f6af39c11171d93a2c01ea
Andersone1347 <Panov.deadman@yandex.ru>
2022-06-21 15:47:36 +0300

    ta

---
+++ main.js
@@ -0,0 +1,10 @@
+const tableBitrix = {
+    title: 'Главный модуль', 
+    description: '',
+    link: 'https://dev.1c-bitrix.ru/api_help/main/index.php'
+},
+document.querySelector('.content').innerHTML = `<table class="bitrix"></table>`
+for(key in tableBitrix) {
+    let row = document.create
+}
+

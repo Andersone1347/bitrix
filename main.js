84b62d35cba125199871bf4e0f56bc4a1883d342
Andersone1347 <panov.deadman@yandex.ru>
2022-06-24 18:04:17 +0300

    3модуля через массивы

--- main.js
+++ main.js
@@ -4,21 +4,21 @@ let bit = [ [
     ["link","https://dev.1c-bitrix.ru/api_help/main/index.php"]
 ],
 [
-    ["title","Главный модуль"],
+    ["title","Сайты"],
     ["description",""],
-    ["link","https://dev.1c-bitrix.ru/api_help/main/index.php"]
+    ["link","#"]
+],
+[
+  ["title","Управление структурой"],
+  ["description",""],
+  ["link","https://dev.1c-bitrix.ru/api_help/fileman/index.php"]
 ],
 ]
-
-
-
-
 document.body.innerHTML = `
 <ol>
   <li><a href='https://dev.1c-bitrix.ru/api_help/main/index.php'>${bit[0][0][1]}</a></li>
-  <li><a href='https://dev.1c-bitrix.ru/api_help/main/index.php'>${bit[1][0][1]}</a></li>
-  
+  <li><a href='#'>${bit[1][0][1]}</a></li>
+  <li><a href='https://dev.1c-bitrix.ru/api_help/fileman/index.php'>${bit[2][0][1]}</a></li>
 </ol>
   `
-
   console.log(document.querySelector('body').innerHTML)

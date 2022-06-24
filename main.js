b51feb69c41e2eaed18a1807e4e67cd43db045a6
Andersone331091 <johndoe@example.com>
2022-06-24 16:45:25 +0300

    вывод с помощью массивов

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
-const tableBitrix = {
-    title: 'Главный модуль', 
-    description: '',
-    link: 'https://dev.1c-bitrix.ru/api_help/main/index.php'
-},
-document.querySelector('.content').innerHTML = `<table class="bitrix"></table>`
-for(key in tableBitrix) {
-    let row = document.create
-}
+let bit = [ [
+    ["title","Главный модуль"],
+    ["description",""],
+    ["link","https://dev.1c-bitrix.ru/api_help/main/index.php"]
+],
+[
+    ["title","Главный модуль"],
+    ["description",""],
+    ["link","https://dev.1c-bitrix.ru/api_help/main/index.php"]
+],
+]
 
+
+
+
+document.body.innerHTML = `
+<ol>
+  <li><a href='https://dev.1c-bitrix.ru/api_help/main/index.php'>${bit[0][0][1]}</a></li>
+  <li><a href='https://dev.1c-bitrix.ru/api_help/main/index.php'>${bit[1][0][1]}</a></li>
+  
+</ol>
+  `
+
+  console.log(document.querySelector('body').innerHTML)
